efd4c28918367115611ef93ca6b47a915fe623b2
Adam Pawliński <adam.pawlinski@gmail.com>
2018-03-07 06:14:23 +0100

    finish task 9.4

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function drawTree (height) {
+  for (var i = 1; i <= height; i++) {
+	var star = "*";
+	var space = "";
+	for (k = height; k > i; k--){
+		space += " ";
+	}
+	for (var j = 2; j <= i; j++){
+		star += "**";
+	}
+	console.log(space + star);
+  }
+}
+drawTree(5);
